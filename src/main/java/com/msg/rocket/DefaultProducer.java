f8c0a46d4d2a454824aefc043ceed940bc2b2b55
SmileHi <736996211@qq.com>
2020-02-03 18:26:10 +0800

    mq

---
+++ src/main/java/com/msg/rocket/DefaultProducer.java
@@ -0,0 +1,18 @@
+package com.msg.rocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+     SpringBoot如何优雅的使用RocketMQ ：https://www.cnblogs.com/SimpleWu/p/12112351.html   -- 其实看似复杂是综合了多个方面 设计  思考的结果 所以分而治之
+     问题：1.为什么配置mq的时候rocket没有提示信息？
+     下午：实现对应的rocket
+     1.十分钟入门RocketMQ      http://jm.taobao.org/2017/01/12/rocketmq-quick-start-in-10-minutes/  结合AmqMsgContainerFactory
+ */
+
+@Configuration
+public class DefaultProducer {
+
+    /*@Bean
+    public Default*/
+}
