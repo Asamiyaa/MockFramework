1cbeaf717b06dc123419ca4c07bb42a7e57be4f6
SmileHi <736996211@qq.com>
2020-04-15 23:24:55 +0800

    nionionio

--- src/main/java/com/pattern/k/execute/TaskService.java
+++ src/main/java/com/pattern/k/execute/TaskService.java
@@ -3,14 +3,18 @@ package com.pattern.k.execute;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import sun.misc.IOUtils;
 
 public class TaskService implements ApplicationContextAware{
+
+    //还有一个调度模块将保存在库中的信息反序列化 进行调用处理
+
 
     //执行任务
     //保存任务
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+//
     }
 }
