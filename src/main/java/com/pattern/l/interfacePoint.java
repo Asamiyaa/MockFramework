4a85e2c70d3b5b881a0231133eeae1f36803ff19
SmileHi <736996211@qq.com>
2020-06-27 17:16:02 +0800

    接口设计注意点

---
+++ src/main/java/com/pattern/l/interfacePoint.java
@@ -0,0 +1,25 @@
+package com.pattern.l;
+
+/**
+ *
+ *  接口设计。。极客仿真接口思考 - 取舍
+         https://blog.csdn.net/qq_35860138/article/details/88783476
+         https://blog.csdn.net/weixin_34414196/article/details/92105613
+         https://www.jianshu.com/p/101bfdc28be6
+         https://www.cnblogs.com/wangjiming/p/10256546.html
+         https://www.jianshu.com/p/5592551c007d
+         https://zhuanlan.zhihu.com/p/136203933
+         https://zhuanlan.zhihu.com/p/130357540
+         https://zhuanlan.zhihu.com/p/144810155
+         https://juejin.im/post/5dfe2e72518825125f39a2de#heading-1
+         https://zhuanlan.zhihu.com/p/139000771
+         https://zhuanlan.zhihu.com/p/148144928
+ */
+
+
+public class interfacePoint {
+
+
+
+
+}
